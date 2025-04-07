953b520194b26f0a7aae3ddf79003884bd98e60d
dan1kali <dl74@rice.edu>
2025-04-07 12:31:38 -0500

    small changes, working

--- backend/app.py
+++ backend/app.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, send_from_directory
+from flask_socketio import SocketIO, emit
 from flask_cors import CORS
+import os
 
-app = Flask(__name__)
-CORS(app)  # This will allow all origins by default
+# app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/build')
 socketio = SocketIO(app)
+CORS(app)  # This will allow all origins by default
 
 @app.route('/')
-#def hello():
-#    return 'Hello from Flask!'
 def index():
-   return render_template('index.html')
+    return send_from_directory(app.static_folder, 'index.html')
+#    return 'Hello from Flask!'
 
 @socketio.on('update_data')
 def handle_update_data(data):
