8fca85b8ff9652b6bb8217241021c309697b2fed
dan1kali <dl74@rice.edu>
2025-04-07 02:10:35 -0500

    set up websocket

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)  # This will allow all origins by default
+socketio = SocketIO(app)
 
 @app.route('/')
-def hello():
-    return 'Hello from Flask!'
+#def hello():
+#    return 'Hello from Flask!'
+def index():
+   return render_template('index.html')
+
+@socketio.on('update_data')
+def handle_update_data(data):
+    # Emit the data to the frontend (React)
+    socketio.emit('new_data', data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
+#     app.run(debug=True)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+/* import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -18,6 +18,33 @@ function App() {
           Learn React
         </a>
       </header>
+    </div>
+  );
+}
+
+export default App;
+ */
+
+
+
+import React, { useEffect, useState } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5000');  // Flask backend
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    socket.on('new_data', (receivedData) => {
+      setData(receivedData);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Arduino Data</h1>
+      <p>{data}</p>
     </div>
   );
 }
