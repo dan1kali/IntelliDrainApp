bdf57f82bb082b9d38b505a26aa60db8af5c2383
dan1kali <dl74@rice.edu>
2025-04-08 03:46:38 -0500

    added containers for all variables to measure in App.py backend

--- backend/app.py
+++ backend/app.py
@@ -19,15 +19,33 @@ def get_current_datetime():
     now = datetime.now()
     return now.strftime("%m/%d/%Y %H:%M:%S")
 
-# Simulate sensor data
-def get_sensor_data():
-    return {"sensor_value": round(random.random() * 100, 3), "date": get_current_datetime()}
+def get_occlusion_sensor_value():
+    # INSERT FUTURE CODE HERE
+    return round(random.random() * 100, 2)
+
+def get_fluid_tracking_values():
+    # INSERT FUTURE CODE HERE
+    saline_volume = round(random.random() * 100, 2)
+    drainage_volume = round(random.random() * 100, 2)
+    return saline_volume, drainage_volume
+
+def get_flush_initiation_times():
+    # INSERT FUTURE CODE HERE
+    return round(random.random() * 100, 2)
+
+# Put data all together in an array
+def get_data():
+    return {"date": get_current_datetime(), 
+            "sensor_value": get_occlusion_sensor_value(), 
+            "saline_volume": get_fluid_tracking_values()[0], 
+            "drainage_volume": get_fluid_tracking_values()[1],
+            "flush_times": get_flush_initiation_times()}
 
 # Background thread that sends sensor data to clients
 def background_thread():
     print("Background thread started", flush=True)  # Add logging here to verify the thread is running
     while True:
-        data = get_sensor_data()
+        data = get_data()
         print(f"Emitting data: {data}")  # Log emitted data to the console for debugging
         socketio.emit('new_data', data)
         socketio.sleep(1)  # Emit data every second
