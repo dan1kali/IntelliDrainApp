0fe9ac10354e6163a1ab286835ba1d4d096b4a4d
dan1kali <dl74@rice.edu>
2025-04-17 00:51:02 -0500

    fixed merge errors

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,11 +9,6 @@ ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, T
 var socket = io('http://localhost:5000', {
   transports: ['websocket'],  // Use WebSocket transport for faster and more stable communication
 });
-<<<<<<< HEAD
-
-=======
-  
->>>>>>> ec43838daba7a4ce1c0e21115d898bf1031ca8e5
 function App() {
   const [data, setData] = useState({
     salineVolumes: [],
@@ -21,7 +16,6 @@ function App() {
     flushTimes: [],
     sensorValues: [],
   });
-<<<<<<< HEAD
 
   const [timestamps, setTimestamps] = useState([]);
   const [status, setStatus] = useState('stopped'); // Track the status (stopped, started, paused)
@@ -36,10 +30,6 @@ function App() {
 
   const [showStopPrompt, setShowStopPrompt] = useState(false); // State for stop prompt
   const [isDataSaving, setIsDataSaving] = useState(false); // Flag for data saving in process
-=======
-  const [labels, setLabels] = useState([]);
-  const [status, setStatus] = useState('stopped'); // Track the status (stopped, started, paused)
->>>>>>> ec43838daba7a4ce1c0e21115d898bf1031ca8e5
 
   useEffect(() => {
     // Establish socket event listeners when the component mounts
@@ -108,13 +98,9 @@ function App() {
         ]);
       }
     });
-<<<<<<< HEAD
     
   
     // Cleanup the socket event listeners when the component unmounts or the status changes
-=======
-
->>>>>>> ec43838daba7a4ce1c0e21115d898bf1031ca8e5
     return () => {
       socket.off('new_data');
       socket.off('connect');
@@ -123,7 +109,6 @@ function App() {
   }, [status]);  // Effect depends on `status`
   
 
-<<<<<<< HEAD
 
 
 
@@ -201,21 +186,6 @@ function App() {
     });
     setTimestamps([]);
     setShowStopPrompt(false);
-=======
-  const handleStart = () => {
-    setStatus('started');  // Update status to 'started'
-    // Add any start logic here (like initiating data fetching or starting a process)
-  };
-
-  const handlePause = () => {
-    setStatus('paused');  // Update status to 'paused'
-    // Add any pause logic here (like pausing data fetching or halting a process)
-  };
-
-  const handleStop = () => {
-    setStatus('stopped');  // Update status to 'stopped'
-    // Add any stop logic here (like stopping data fetching or halting a process)
->>>>>>> ec43838daba7a4ce1c0e21115d898bf1031ca8e5
   };
 
   const handleDownload = () => {
@@ -326,14 +296,9 @@ function App() {
           color: 'rgba(17, 56, 100, 1)',
         },
         ticks: {
-<<<<<<< HEAD
           color: 'rgba(17, 56, 100, 1)',
           maxTicksLimit: 10,
         },
-=======
-          color: 'rgba(106, 90, 205, 1)',  // Tick mark color (purple)
-          autoSkip: true,        },
->>>>>>> ec43838daba7a4ce1c0e21115d898bf1031ca8e5
         grid: {
           color: 'rgba(216, 191, 216, 0.5)',
         },
@@ -380,7 +345,6 @@ function App() {
             </select>
           </div> */}
 
-<<<<<<< HEAD
 
           <div className="controlsContainer">
             <label htmlFor="device-select">Select Port: </label>
@@ -418,32 +382,12 @@ function App() {
             >
               Rescan Ports
             </button>
-=======
-          <div className="dataDisplay">
-            <h3>Fluid Tracking</h3>
-            <p>Total Drainage: {data.drainageVolumes[data.drainageVolumes.length - 1]}</p>
-            <p>Total Flush: {data.salineVolumes[data.salineVolumes.length - 1]}</p>
-            <p>Net Volume Drained: {(data.drainageVolumes[data.drainageVolumes.length - 1] - data.salineVolumes[data.salineVolumes.length - 1]).toFixed(2)}</p>
-          </div>
-
-          <div className="scrollingData">
-            <h3>Flushing Timestamps</h3>
-            {data.flushTimes.map((flushTime, index) => (
-              <p key={index}>{flushTime}</p>
-            ))}
->>>>>>> ec43838daba7a4ce1c0e21115d898bf1031ca8e5
           </div>
 
           <div className="controlsContainer">
-<<<<<<< HEAD
             <button onClick={handleStartButton} disabled={status === 'started'}>Start</button>
             <button onClick={handlePauseButton} disabled={status !== 'started'}>Pause</button>
             <button onClick={handleStopButton} disabled={status === 'stopped'}>Stop</button>
-=======
-            <button onClick={handleStart} disabled={status === 'started'}>Start</button>
-            <button onClick={handlePause} disabled={status !== 'started'}>Pause</button>
-            <button onClick={handleStop} disabled={status === 'stopped'}>Stop</button>
->>>>>>> ec43838daba7a4ce1c0e21115d898bf1031ca8e5
           </div>
 
           <div className="patientDataContainer">
@@ -458,7 +402,6 @@ function App() {
               />
             </div>
 
-<<<<<<< HEAD
             <div className="formGroup">
               <label>Sex: </label>
               <select
@@ -480,13 +423,6 @@ function App() {
                 onChange={handleInputChange}
               />
             </div>
-=======
-        <div className="rightColumn">
-
-          <div className="chartContainer">
-            <h3>Sensor Output</h3>
-            <Line data={chartData} options={chartOptions} />
->>>>>>> ec43838daba7a4ce1c0e21115d898bf1031ca8e5
           </div>
 
           <button className="downloadButton" onClick={handleDownload}>
